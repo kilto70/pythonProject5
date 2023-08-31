643483ccede065228ddb1b9afc84880291bd83fb
kilto70 <kilto70@ya.ru>
2023-09-01 00:50:38 +0300

    ffff

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-def calculate_ticket_price(age):
-    if age < 18:
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return low
+
+
+def insert_position(arr, target):
+    index = binary_search(arr, target)
+    if index < len(arr) and arr[index] == target:
+        return index
+    elif index == 0:
         return 0
-    elif age < 25:
-        return 990
+    elif index == len(arr):
+        return len(arr)
     else:
-        return 1390
+        return index if target - arr[index - 1] <= arr[index] - target else index - 1
 
 
-def calculate_total_price(num_tickets):
-    total_price = 0
-    for i in range(num_tickets):
-        age = int(input(f"Введите возраст посетителя {i + 1}-го билета: "))
-        total_price += calculate_ticket_price(age)
-    if num_tickets > 3:
-        total_price *= 0.9
-    return total_price
-
-
-num_tickets = int(input("Введите количество билетов: "))
-total_price = calculate_total_price(num_tickets)
-print(f"Сумма к оплате: {total_price} руб.")
+numbers = list(map(int, input("Enter a sequence of numbers separated by spaces: ").split()))
+numbers.sort()
+target = int(input("Enter a number to find its insert position: "))
+result = insert_position(numbers, target)
+print(f"The insert position for {target} is {result}")
